533c3cb28581b5461623b245c7358166ba92919b
Sergey <tootoo9723@gmail.com>
2021-03-05 14:40:53 +0300

    Подправил оформление в задании по ветвям.
    Перенес инициализацию данных из метода в полученные методом переменные.

--- ConsoleApp1/HwIfElse.cs
+++ ConsoleApp1/HwIfElse.cs
@@ -7,16 +7,11 @@ namespace ConsoleApp1
     public static class HwIfElse
 
     {
-        public static int Task1()
+        public static int Task1(int a, int b)
         {
             // 1 Пользователь вводит 2 числа (A и B). Если A>B, подсчитать A+B, если A=B, подсчитать A*B, если A<B, подсчитать A-B.
-            int a;
-            int b;
-            int res;
-            Console.Write("Enter A ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter B ");
-            b = Convert.ToInt32(Console.ReadLine());
+            
+            int res;           
             if (a > b)
             {
                 res = a + b;
@@ -36,17 +31,9 @@ namespace ConsoleApp1
         }
 
 
-        public static string Task2()
+        public static string Task2(int x, int y)
         {
-
             // 2 Пользователь вводит 2 числа (X и Y). Определить какой четверти принадлежит точка с координатами (X,Y).
-
-            int x;
-            int y;
-            Console.Write("Enter X ");
-            x = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Y ");
-            y = Convert.ToInt32(Console.ReadLine());
 
             if (x > 0)
             {
@@ -62,20 +49,10 @@ namespace ConsoleApp1
 
         }
 
-        public static string Task3()
+        public static string Task3(int a, int b, int c)
         {
             // 3 Пользователь вводит 3 числа (A, B и С). Выведите их в консоль в порядке возрастания.
-
-            int a;
-            int b;
-            int c;
-            Console.Write("Enter A ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter B ");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter C ");
-            c = Convert.ToInt32(Console.ReadLine());
-
+                      
             if (a > b && a > c)
             {
                 if (b > c)
@@ -92,28 +69,21 @@ namespace ConsoleApp1
             }
             else if (a > b)
                 return ($"{b} {a} {c}");
+            else 
+                return ($"{a} {b} {c}");
         }
 
 
 
-        public static double[] Task4()
+        public static double[] Task4(double a, double b, double c)
         {
             // 4 Пользователь вводит 3 числа(A, B и С).Выведите в консоль решение(значения X) квадратного уравнения стандартного вида, где AX2 + BX + C = 0.
-
-            double a;
-            double b;
-            double c;
+                        
             double d;
             double x1;
             double x2;
             double[] result = new double[2];
-            Console.Write("Enter A ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter B ");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter C ");
-            c = Convert.ToInt32(Console.ReadLine());
-
+           
             if (a != 0)
             {
                 d = b * b - (4 * a * c);
