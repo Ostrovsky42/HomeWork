6ae1200e1cc55ccca568ace896dc5055f31c74af
Sergey <tootoo97@gmail.com>
2021-03-07 11:56:50 +0300

    Добавил тесты в задания по переменным и ветвям

--- ConsoleApp1/HwIfElse.cs
+++ ConsoleApp1/HwIfElse.cs
@@ -7,13 +7,30 @@ namespace ConsoleApp1
     public static class HwIfElse
 
     {
-      
-        
+        public static void Main()
+        {
+            Random random = new Random();
+            int a = 10;//random.Next(10,99);
+           // int b = random.Next();
+           // int c = random.Next();
+            string ar;
+            for (int i = 0; i < 89; i++)
+            {
+
+                ar = Task5(a);
+                Console.WriteLine(ar);
+                a++;
+               // b++;
+
+
+            }
+        }
+
         public static int Task1(int a, int b)
         {
             // 1 Пользователь вводит 2 числа (A и B). Если A>B, подсчитать A+B, если A=B, подсчитать A*B, если A<B, подсчитать A-B.
-            
-            int res;           
+
+            int res;
             if (a > b)
             {
                 res = a + b;
@@ -36,6 +53,14 @@ namespace ConsoleApp1
         public static string Task2(int x, int y)
         {
             // 2 Пользователь вводит 2 числа (X и Y). Определить какой четверти принадлежит точка с координатами (X,Y).
+            if (x == 0 || y == 0)
+            {
+                throw new Exception("Точка лежит в начале коорднат");
+            }
+            if(x == 0 && y == 0)
+            {
+                throw new Exception("Точка лежит на оси коорднат");
+            }
 
             if (x > 0)
             {
@@ -54,7 +79,7 @@ namespace ConsoleApp1
         public static string Task3(int a, int b, int c)
         {
             // 3 Пользователь вводит 3 числа (A, B и С). Выведите их в консоль в порядке возрастания.
-                      
+
             if (a > b && a > c)
             {
                 if (b > c)
@@ -71,7 +96,7 @@ namespace ConsoleApp1
             }
             else if (a > b)
                 return ($"{b} {a} {c}");
-            else 
+            else
                 return ($"{a} {b} {c}");
         }
 
@@ -80,12 +105,12 @@ namespace ConsoleApp1
         public static double[] Task4(double a, double b, double c)
         {
             // 4 Пользователь вводит 3 числа(A, B и С).Выведите в консоль решение(значения X) квадратного уравнения стандартного вида, где AX2 + BX + C = 0.
-                        
+
             double d;
             double x1;
             double x2;
             double[] result = new double[2];
-           
+
             if (a != 0)
             {
                 d = b * b - (4 * a * c);
@@ -115,48 +140,49 @@ namespace ConsoleApp1
         {     //Пользователь вводит двузначное число. Выведите в консоль прописную запись этого числа. Например при вводе “25” в консоль будет выведено “двадцать пять”.
 
             string answer;
-            string[] stringToTen = new string[10] {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
-            string[] stringAfterTen = new string[10] { "", "десять " , "двадцать ", "тридцать ", "сорок ", "пятьдсят ", "шестьдесят ", "семьдесят ", "восемдесят ", "девяносто " };
+            string[] stringToTen = new string[10] { "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+            string[] stringAfterTen = new string[10] { "", "десять ", "двадцать ", "тридцать ", "сорок ", "пятьдсят ", "шестьдесят ", "семьдесят ", "восемдесят ", "девяносто " };
             string[] stringTenToTwenty = new string[10] { "", "одинадцать ", "двенадцать ", "тринадцать ", "четырнадцать ", "пятьнадцать ", "шестьнадцать ", "семьнадцать ", "восемнадцать ", "девятнадцать " };
-            
-            if (num / 10 == 1&&num<20)
+
+            if (num / 10 == 1 && num < 20)
             {
                 switch (num % 10)
-                { 
+                {
                     case 0:
                         return stringTenToTwenty[0];
                     case 1:
-                        return stringTenToTwenty[1];                        
+                        return stringTenToTwenty[1];
                     case 2:
-                        return stringTenToTwenty[2];                      
+                        return stringTenToTwenty[2];
                     case 3:
-                        return stringTenToTwenty[3];                        
+                        return stringTenToTwenty[3];
                     case 4:
-                        return stringTenToTwenty[4];                      
+                        return stringTenToTwenty[4];
                     case 5:
-                        return stringTenToTwenty[5];                     
+                        return stringTenToTwenty[5];
                     case 6:
-                        return stringTenToTwenty[6];                      
+                        return stringTenToTwenty[6];
                     case 7:
-                        return stringTenToTwenty[7];                    
+                        return stringTenToTwenty[7];
                     case 8:
-                        return stringTenToTwenty[8];                        
+                        return stringTenToTwenty[8];
                     case 9:
-                        return stringTenToTwenty[9];                    
-                                                                                            //нечестно вышло
+                        return stringTenToTwenty[9];
+                        //нечестно вышло
                 }
-            }answer = stringAfterTen[num / 10] + stringToTen[num % 10];
+            }
+            answer = stringAfterTen[num / 10] + stringToTen[num % 10];
             return answer;
-           
+
         }
-       
+
 
 
 
 
     }
 
-            
 
 
+}
     
--- ConsoleApp1/HwVar.cs
+++ ConsoleApp1/HwVar.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 
-namespace ConsoleApp4
+namespace ConsoleApp1
 {
     class HwVar
     {
-      
 
+        public static void Main()
+        {
+           
+        }
 
 
         public static int Task1(int a, int b)
-        {                  
-            a = (5 * a + b * b) / (b - a);          
+        { //1 Пользователь вводит 2 числа (A и B). Выведите в консоль решение (5*A+B^2)/(B-A)
+            if (b - a == 0)
+            {
+                throw new Exception("Деление на ноль");
+            }
+                      
+            a = (5 * a + b * b) / (b - a);         
+            
             return a;
         }
 
         public static int[] Task2(int a,  int b)
-        {
+        { // 2 Пользователь вводит 2 значения(A и B). Поменяйте содержимое переменных A и B местами.
             int[] result = new int[2];
+            
             int temporary = a;            
             a = b;
             b = temporary;
@@ -29,7 +39,11 @@ namespace ConsoleApp4
 
         public static int[] Task3(int a, int b)
         {
-
+            // 3 Пользователь вводит 2 числа (A и B). Выведите в консоль результат деления A на B и остаток от деления.
+            if (b == 0)
+            {
+                throw new Exception("Деление на ноль");
+            }
             int[] division = new int[2];
             division[0] = a / b;
             Console.WriteLine("division result = " + division[0]);
@@ -39,24 +53,35 @@ namespace ConsoleApp4
         }
 
         public static double Task4(int a, int b, int c)
-        {            
+        {            // 4 Пользователь вводит 3 числа (A, B и С). Выведите в консоль решение(значение X) линейного уравнения стандартного вида, где A*X+B=C.
+            if (a == 0)
+            {
+                throw new Exception("Деление на ноль");
+            }
             double x;           
             x = (c - b) / a;
             Console.WriteLine("X= " + x);
             return x;
         }
      public   static int[] Task5(int x1, int x2,  int y1, int y2)
-        {           
+        {           // 5 Пользователь вводит 4 числа (X1, Y1, X2, Y2), описывающие координаты 2-х точек на координатной плоскости. Выведите уравнение прямой в формате Y=AX+B, проходящей через эти точки.
             int a;
             int b;
             int[] result = new int[2];
-            
+            if (x1 - x2 == 0)
+            {
+                throw new Exception("Деление на ноль");
+            }
             a = (y1 - y2) / (x1 - x2);
             b = y2 - a * x2;
             Console.Write("y=" + a + "x+" + b);
             result[0] = a;
             result[1]= b;
             return result;
+        }
+        
+
+
         }
 
     
@@ -71,4 +96,3 @@ namespace ConsoleApp4
     
     
     }
-}
